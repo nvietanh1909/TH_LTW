d73801a14724f2a53c566b357bfe109fd59b2326
nvietanh1909 <nvietanh.190904@example.com>
2023-09-30 09:34:24 +0700

    Thuc Hanh

---
+++ DemoWeb/DemoWeb/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using DemoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        DBSportStoreEntities database = new DBSportStoreEntities();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = database.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            try
+            {
+                database.Categories.Add(category);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("LỖI TẠO MỚI CATEGORY");
+            }
+        }
+    }
+}
